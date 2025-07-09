9119c78fbe6d50274e125cb6c58aaf38e465d24f
emre-mentes <emreowen57@gmail.com>
2025-07-09 15:00:41 +0200

    Eson

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("baba sarki yapti ulkeyi salladi");
         System.out.println("Hello, World!, merhabaaaaaaa");
         System.out.println("Hello, World!, Çiğdem");
         System.out.println("bebiştay");
